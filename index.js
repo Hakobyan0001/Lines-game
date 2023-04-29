1c8e99ac99aca694668b1d6563c29038e1e54750
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2023-04-29 11:49:08 +0400

    added 3 balls with different colors

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const board = document.querySelector('#board');
-        
+const board = $('#board');
+const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
+const cubes = $('.cube');
+
 for (let i = 0; i < 81; i++) {
-    const cube = document.createElement('div');
-    cube.classList.add('cube');
-    board.appendChild(cube);
+  const cube = $('<div></div>').addClass('cube');
+  board.append(cube);
+  cubes.push(cube);
 }
+
+let indices = [];
+while (indices.length < 3) {
+  let index = Math.floor(Math.random() * 81);
+  if (!indices.includes(index)) {
+    indices.push(index);
+  }
+}
+
+cubes.each(function(index) {
+    if (indices.includes(index)) {
+        const ballColor = colors[Math.floor(Math.random() * colors.length)];
+        $(this).append('<div class="ball ' + ballColor + '"></div>');
+    }
+});
+
