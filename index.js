72674c7bf7acd1d3674bdadf9432d022c393fe35
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2023-04-29 12:44:22 +0400

    added 3 random balls every 2 seconds

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const board = $('#board');
 const colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange'];
 const cubes = $('.cube');
+let indices = [];
 
 for (let i = 0; i < 81; i++) {
   const cube = $('<div></div>').addClass('cube');
@@ -8,18 +9,22 @@ for (let i = 0; i < 81; i++) {
   cubes.push(cube);
 }
 
-let indices = [];
-while (indices.length < 3) {
-  let index = Math.floor(Math.random() * 81);
-  if (!indices.includes(index)) {
-    indices.push(index);
-  }
-}
-
-cubes.each(function(index) {
-    if (indices.includes(index)) {
-        const ballColor = colors[Math.floor(Math.random() * colors.length)];
-        $(this).append('<div class="ball ' + ballColor + '"></div>');
+function addBalls() {
+  const newIndices = [];
+  while (newIndices.length < 3) {
+    let index = Math.floor(Math.random() * 81);
+    if (!indices.includes(index) && !newIndices.includes(index)) {
+      newIndices.push(index);
     }
-});
+  }
 
+  cubes.each(function (index) {
+    if (newIndices.includes(index)) {
+      const ballColor = colors[Math.floor(Math.random() * colors.length)];
+      $(this).append('<div class="ball ' + ballColor + '"></div>');
+    }
+  });
+  indices = indices.concat(newIndices);
+}
+addBalls();
+setInterval(addBalls, 2000);
