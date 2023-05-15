4db470b7fcd778c5cdd7748b9d30538c31990242
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2023-05-15 21:10:41 +0400

    problem in using random numbers

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 let board;
 const COLORS = ['red', 'green', 'blue'];
-const BALLS_INDICES = [];
-
+let emptyCelssIndices = [];
+let emptyCells = [];
+// filter, map imanal inca anum
+// randomi harcy lucel, 
 // creating board
 function createBoard(boardLength) {
     board = new Array(boardLength ** 2).fill(null);
+    emptyCelssIndices = board.map((element, index) => index);
     initBoardview();
 }
 
 // show board in page
 function initBoardview() {
+    //array
     board.forEach(function (cell, index) {
         const CUBE_ELEMENT = $('<div></div>').attr({
             'class': 'cube',
@@ -25,45 +29,52 @@ function updateBoardView() {
         if (ball) {
             const BALL_ELEMENT = $('<div class="ball ' + ball.color + '"></div>')
             $("#" + index).append(BALL_ELEMENT);
-
+            emptyCelssIndices.splice(index, 1);
         }
+
     })
+    console.log(emptyCelssIndices);
 
 }
 
 // creating balls and pushing in board
 function createRandomBalls(ballsNumber) {
-    while (BALLS_INDICES.length < ballsNumber) {
-        let randomIndex = getRandomIndex();
-        if (!BALLS_INDICES.includes(randomIndex)) {
-            BALLS_INDICES.push(randomIndex);
-        }
-    }
-    $(board).each(function (index) {
-        if (BALLS_INDICES.includes(index)) {
-            const ADD_COLOR = getRandomColor();
-            let ball = {
-                color: ADD_COLOR,
-                isActive: false
-            }
-            board[index] = ball
-        }
-    });
+    let i = 0;
+    while (i < ballsNumber) {
 
+
+        const RANDOM_INDEX = getRandomIndex();
+        const RANDOM_COLOR = COLORS[getRandomColor()];
+        const BALL = {
+            color: RANDOM_COLOR,
+            isActive: false
+        }
+        board[RANDOM_INDEX] = BALL
+        i++;
+    }
 }
+
 function getRandomNumber(limit) {
     let ballsNumber = Math.floor(Math.random() * limit);
     return ballsNumber;
 }
 
 function getRandomColor() {
-    let ballColor = COLORS[Math.floor(Math.random() * COLORS.length)];
+    let ballColor = getRandomNumber(COLORS.indexOf('red'), COLORS.length - 1);
     return ballColor;
 }
 
 function getRandomIndex() {
-    let index = Math.floor(Math.random() * board.length);
-    return index;
+    emptyCells = emptyCelssIndices.filter(cell => cell)
+    // console.log(emptyCells);
+    const RANDOM_INDEX = getRandomNumber(emptyCells[0], emptyCells.length - 1);
+    return RANDOM_INDEX;
+}
+
+function getRandomNumber(from, to) {
+    let min = Math.ceil(from);
+    let max = Math.floor(to);
+    return Math.floor(Math.random() * (max - min + 1) + min);;
 }
 
 function renderBall(ball, index) {
@@ -80,9 +91,21 @@ function moveBall(index) {
 
 }
 
-function selectBall(color, index) {
+// function selectBall() {
+//     let selectedBall;
+//     $("#board").on('click', function (ball, index) {
+//         const BALL = $("#" + index).find('.ball');
+//         debugger;
+//         if (selectedBall && !BALL.length) {
+//             $(this).append(selectedBall);
+//             selectedBall = null;
+//         }
+//         else if (BALL.length && !selectedBall) {
+//             selectedBall = BALL.detach();
+//         }
 
-}
+//     });
+// }
 
 function disSelectBall(index) {
 
@@ -102,7 +125,7 @@ function startGame() {
     updateBoardView();
     // addBall(color, index);
     // removeBall(index);
-    // selectBall(color, index);
+    // selectBall();
     // disSelectBall(color, index);
 }
 
