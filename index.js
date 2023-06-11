6f1b99a3888c6d42ae5bb440d386a22e2928c5fa
Hakobyan0001 <123662211+Hakobyan0001@users.noreply.github.com>
2023-06-11 15:26:18 +0400

    add f() removing diagonal lines

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let board;
 let boardLength = 9;
-let randomBallsCount = 3;
 const COLORS = ['red', 'green', 'blue'];
 let emptyCellsIndices = [];
 let selectedBallIndex;
 let gamePoint = 0;
+const REQUIRED_NUMBER_OF_BALLS = 5;
 
 // creating board
 function createBoard(boardLength) {
@@ -32,6 +32,7 @@ function initBoardview() {
 // show balls in page
 function updateBoardView() {
     board.forEach(function (ball, index) {
+
         if (ball) {
             if (ball.isActive === false) {
                 $("#" + index).empty();
@@ -53,6 +54,7 @@ function updateBoardView() {
 // creating balls and pushing in board
 function createRandomBalls(ballsNumber) {
     let i = 0;
+
     while (i < ballsNumber) {
         const RANDOM_INDEX = getRandomIndex();
         const RANDOM_COLOR = getRandomColor();
@@ -90,6 +92,7 @@ function getRandomNumber(min, max) {
 // checking the clicked cube and perform an action
 function handelCellClick(index) {
     const BALL = board[index]
+
     if (!BALL && selectedBallIndex === undefined) {
         return;
     }
@@ -109,6 +112,8 @@ function handelCellClick(index) {
         updateBoardView();
         removeHorizontalBallsLine();
         removeVerticalBallsLine();
+        removeDiagonalBallsLine1();
+        removeDiagonalBallsLine2();
         return;
     }
 }
@@ -126,18 +131,19 @@ function moveBall(selectedBallIndex, clickedCellIndex) {
     emptyCellsIndices.push(selectedBallIndex);
     emptyCellsIndices = emptyCellsIndices.filter(el => el !== clickedCellIndex);
     disSelectBall(board[clickedCellIndex]);
-    createRandomBalls(randomBallsCount);
+    createRandomBalls(COLORS.length - 1);
 
 }
 
 // removing horizantal same colored balls line
 function removeHorizontalBallsLine() {
-    const REQUIRED_NUMBER_OF_BALLS = 5;
-    let checkedBallsColor = null;
-    let ballsCount = 0;
     for (let row = 0; row < boardLength; row++) {
+        let checkedBallsColor = null;
+        let ballsCount = 0;
+
         for (let col = 0; col < boardLength; col++) {
             const BALL = board[row * boardLength + col];
+
             if (BALL && BALL.color === checkedBallsColor) {
                 ballsCount++;
             } else {
@@ -145,6 +151,7 @@ function removeHorizontalBallsLine() {
                 checkedBallsColor = BALL ? BALL.color : null;
             }
             if (ballsCount === REQUIRED_NUMBER_OF_BALLS) {
+
                 for (let i = col - ballsCount + 1; i <= col; i++) {
                     let removedBallIndex = row * boardLength + i;
                     emptyCellsIndices.push(removedBallIndex);
@@ -160,12 +167,13 @@ function removeHorizontalBallsLine() {
 
 // removing vertical same colored balls line
 function removeVerticalBallsLine() {
-    const REQUIRED_NUMBER_OF_BALLS = 5;
-    let checkedBallsColor = null;
-    let ballsCount = 0;
     for (let col = 0; col < boardLength; col++) {
+        let checkedBallsColor = null;
+        let ballsCount = 0;
+
         for (let row = 0; row < boardLength; row++) {
             const BALL = board[col + row * boardLength];
+
             if (BALL && BALL.color === checkedBallsColor) {
                 ballsCount++;
             } else {
@@ -173,6 +181,7 @@ function removeVerticalBallsLine() {
                 checkedBallsColor = BALL ? BALL.color : null;
             }
             if (ballsCount === REQUIRED_NUMBER_OF_BALLS) {
+
                 for (let i = row - ballsCount + 1; i <= row; i++) {
                     let removedBallIndex = col + (i * boardLength);
                     emptyCellsIndices.push(removedBallIndex);
@@ -186,14 +195,101 @@ function removeVerticalBallsLine() {
     updateBoardView();
 }
 
-function removeDiagonalBallsLine() {
+// function removeDiagonalBallsLine() {
+//     for (let row = 0; row <= boardLength; row++) {
+//         debugger;
+//         for (let col = 0; col <= boardLength; col++) {
+//             const BALL = board[row * boardLength + col];
+//             if (BALL && BALL.color === checkedBallsColor) {
+//                 ballsCount++;
+//             } else {
+//                 ballsCount = 1;
+//                 checkedBallsColor = BALL ? BALL.color : null;
+//             }
 
+//             if (ballsCount === REQUIRED_NUMBER_OF_BALLS) {
+//                 for (let i = row; i >= 0; i--) {
+//                     let removedBallIndex = row * boardLength + i;
+//                     emptyCellsIndices.push(removedBallIndex);
+//                     board[removedBallIndex] = null;
+//                     gamePoint = gamePoint + 1;
+//                 }
+
+//                 break;
+//             }
+//             row++;
+//         }
+//     }
+// }
+
+// remove diagonal from top left to right bottom
+function removeDiagonalBallsLine1() {
+    for (let row = 0; row <= boardLength - REQUIRED_NUMBER_OF_BALLS; row++) {
+
+        for (let col = 0; col <= boardLength - REQUIRED_NUMBER_OF_BALLS; col++) {
+            let ballsCount = 1;
+            let checkedBallsColor = null;
+
+            for (let angle = 0; angle < REQUIRED_NUMBER_OF_BALLS; angle++) {
+                const BALL = board[(row + angle) * boardLength + (col + angle)];
+
+                if (BALL && BALL.color === checkedBallsColor) {
+                    ballsCount++;
+                } else {
+                    ballsCount = 1;
+                    checkedBallsColor = BALL ? BALL.color : null;
+                }
+
+                if (ballsCount === REQUIRED_NUMBER_OF_BALLS) {
+                    for (let angle = 0; angle < REQUIRED_NUMBER_OF_BALLS; angle++) {
+                        let removedBallIndex = (row + angle) * boardLength + (col + angle);
+                        emptyCellsIndices.push(removedBallIndex);
+                        board[removedBallIndex] = null;
+                        gamePoint++;
+                    }
+                    updateBoardView();
+                    return;
+                }
+            }
+        }
+    }
 }
 
+// remove diagonal from bottom left to top right 
+function removeDiagonalBallsLine2() {
+    for (let row = boardLength - 1; row >= boardLength - REQUIRED_NUMBER_OF_BALLS - 1; row--) {
 
+        for (let col = 0; col <= boardLength - REQUIRED_NUMBER_OF_BALLS; col++) {
+            let ballsCount = 1;
+            let checkedBallsColor = null;
+
+            for (let angle = 0; angle < REQUIRED_NUMBER_OF_BALLS; angle++) {
+                const BALL = board[(row - angle) * boardLength + (col + angle)];
+
+                if (BALL && BALL.color === checkedBallsColor) {
+                    ballsCount++;
+                } else {
+                    ballsCount = 1;
+                    checkedBallsColor = BALL ? BALL.color : null;
+                }
+
+                if (ballsCount === REQUIRED_NUMBER_OF_BALLS) {
+                    for (let angle = 0; angle < REQUIRED_NUMBER_OF_BALLS; angle++) {
+                        let removedBallIndex = (row - angle) * boardLength + (col + angle);
+                        emptyCellsIndices.push(removedBallIndex);
+                        board[removedBallIndex] = null;
+                        gamePoint++;
+                    }
+                    updateBoardView();
+                    return;
+                }
+            }
+        }
+    }
+}
 function startGame() {
-    boardLength = prompt("write the number of vertical and horizontal boxes");
-    randomBallsCount = prompt("write the number of balls you wanted to add");
+    boardLength = parseInt(prompt("write the number of vertical and horizontal boxes"));
+    randomBallsCount = parseInt(prompt("write the number of balls you wanted to add every time"));
     createBoard(boardLength);
     createRandomBalls(randomBallsCount);
 
